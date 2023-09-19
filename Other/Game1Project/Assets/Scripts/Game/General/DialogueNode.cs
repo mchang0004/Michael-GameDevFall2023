49c2949e574e2dc94875fd9b7af66e373880049e
Michael Chang <michaelxchang.ca@gmail.com>
2023-09-19 00:36:11 -0700

    Modified dialogue system
    Created a new scriptable object for dialogue options! Added options buttons to UI

---
+++ Other/Game1Project/Assets/Scripts/Game/General/DialogueNode.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue Node", menuName = "Scriptable object/Dialogue Node")]
+public class DialogueNode : ScriptableObject
+{
+	[TextArea(4, 10)]
+	public string text; // Text displayed
+	public string buttonText; //text that will appear on the button
+	public bool endAfter;	//should the dialogue end after this?
+
+	public DialogueNode option1;
+	public DialogueNode option2;
+	public DialogueNode option3;
+	public DialogueNode option4;
+}
--- Other/Game1Project/Assets/Scripts/UI/DialogueManager.cs
+++ Other/Game1Project/Assets/Scripts/UI/DialogueManager.cs
@@ -9,8 +9,10 @@ public class DialogueManager : MonoBehaviour
 {
 	public bool inDialogue;
 	public TextMeshProUGUI dialogueBox;
-    public GameObject dialogueArea;
-    
+    public GameObject dialogueBoxArea;
+
+    public DialogueNode currentText;
+    public Button option1, option2, option3, option4;
 
     //Things to disable when in dialogue
 	public PlayerMovement playerMovement; 
@@ -20,31 +22,121 @@ public class DialogueManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        setText();
-        inDialogue = false;
+		//setText(); //change later 
+		setText();
+		inDialogue = true;
 
 	}
 
     // Update is called once per frame
     void Update()
     {
-        if (inDialogue)
-        {
-            dialogueArea.SetActive(true);
-            inventory.inventoryShown = playerMovement.canMove = false;
+      
 
-		} else
-        {
-            dialogueArea.SetActive(false);
+		//Show or hide the dialogue text area depending on current state:
+		if (inDialogue)
+		{
+			dialogueBoxArea.SetActive(true);
+			inventory.inventoryShown = playerMovement.canMove = false;
+
+			//This shows the buttons for each option 
+			if (currentText != null)
+			{
+				SetButton(option1, currentText.option1);
+				SetButton(option2, currentText.option2);
+				SetButton(option3, currentText.option3);
+				SetButton(option4, currentText.option4);
+			}
+			else
+			{
+				option1.gameObject.SetActive(false);
+				option2.gameObject.SetActive(false);
+				option3.gameObject.SetActive(false);
+				option4.gameObject.SetActive(false);
+			}
+
+		}
+		else
+		{
+			dialogueBoxArea.SetActive(false);
 			inventory.inventoryShown = playerMovement.canMove = true;
 
 		}
-
-
 	}
 
-    void setText()
+
+
+	//if there is no option, then don't set/show the text/button.  
+
+	private void SetButton(Button button, DialogueNode option)
+	{
+		button.gameObject.SetActive(option != null);
+
+		if (option != null)
+		{
+			TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>(); 
+			
+			if (buttonText != null)
+			{
+				buttonText.text = option.buttonText;
+			}
+
+			button.onClick.RemoveAllListeners();
+			button.onClick.AddListener(() => nextNode(option));
+		}
+	}
+
+		/*void getDialogue()
+		{
+			//finds the NPC/where the dialogue gameobject trigger
+			if(dialogueTrigger != null)
+			{
+
+			} else
+			{
+				Debug.LogWarning("DIALOGUE TRIGGER NOT FOUND");
+			}
+		}*/
+
+		//change later
+
+		public void startDialogue(DialogueNode dialogueNode)
+	{
+		currentText = dialogueNode;
+		inDialogue = true;
+	}
+
+	public void nextNode(DialogueNode nextNode)
+	{
+		if (currentText.endAfter)
+		{
+			endDialogue();
+				
+		} else
+		{
+			currentText = nextNode;
+			setText();
+		}
+		
+	}
+
+	void setText()
+	{
+		Debug.Log(currentText.text);
+		
+		if (dialogueBox != null && currentText != null)
+		{
+			dialogueBox.text = currentText.text;
+		}
+		else
+		{
+			Debug.Log("dialogueBox or currentText is null");
+		}
+	}
+
+	void endDialogue()
     {
-        dialogueBox.text = "Hello! This is a test.";
+        inDialogue = false;
+        
     }
 }
